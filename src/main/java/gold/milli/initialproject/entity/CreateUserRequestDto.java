da8822e2ecd75f4aa6d7e4f42d51c4cc1ac0b514
Artemis-Fowl1381 <arash13811393@gmail.com>
2024-08-24 17:45:25 +0330

    forgot to push (the hash part)

--- src/main/java/gold/milli/initialproject/entity/CreateUserRequestDto.java
+++ src/main/java/gold/milli/initialproject/entity/CreateUserRequestDto.java
@@ -3,8 +3,8 @@ package gold.milli.initialproject.entity;
 import gold.milli.initialproject.entity.enums.Role;
 import jakarta.validation.constraints.*;
 import lombok.*;
-
 import java.util.List;
+import org.apache.commons.codec.digest.DigestUtils;
 @Data
 @AllArgsConstructor
 @ToString
@@ -12,7 +12,7 @@ public class CreateUserRequestDto {
     @NotBlank(message = "Username cannot be blank")
     @Size(max = 50, message = "Username must be less than 50 characters")
     private String username;
-    @Size(min = 8, max = 20, message = "password must be between 6 and 20 characters")
+    @Size(min = 8, message = "password must be at least 6 characters")
     @NotBlank(message = "Password cannot be blank")
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d).+$", message = "Password must contain at least one letter and one number")
     private String password;
@@ -21,4 +21,8 @@ public class CreateUserRequestDto {
     private String email;
     @NotEmpty(message = "You must have a role")
     private List<Role> roles;
+
+    public void setPassword(String password) {
+        this.password = DigestUtils.sha256(password).toString();
+    }
 }
